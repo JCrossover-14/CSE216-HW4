166e3a32ee1a8a2820758ef56e0651d0eac19735
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2023-06-26 19:58:05 -0400

    "Weighted Edge updated"

---
+++ HW4/WeightedEdge.py
@@ -0,0 +1,16 @@
+class WeightedEdge:
+    def __init__(self,node1,node2,weight):
+        self.node1=node1
+        self.node2=node2
+        self.weight=weight
+
+    def getNode1(self):
+        return self.node1
+    
+    def getNode2(self):
+        return self.node2
+    
+    def getWeight(self):
+        return self.weight
+    
+    
