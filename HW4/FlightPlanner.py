f1f36167ba2403ae35b8a32c50179c6fb2efb95c
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2023-06-27 15:04:32 -0400

    Skeleton of FlightPlanner Finished. Some functions still have to implement

--- HW4/FlightPlanner.py
+++ HW4/FlightPlanner.py
@@ -5,4 +5,49 @@ from Stop_Transaction import Stop_Transaction
 from pyTPS import pyTPS
 from pyTPS_Transaction import pyTPS_Transaction
 
+def initAllAirports():
+    return
+    #still left to be implemented
+
+def displayAirports():
+    return 
+
+def displayMenu():
+    return
+
+def displayCurrentTrip():
+    return 
+
+
+
 tps = pyTPS()
+print(tps.toString())
+
+airportGraph = WeightedGraph()
+distances = []
+
+stops =[]
+initAllAirports()
+keepGoing = True
+while(keepGoing):
+    displayAirports()
+    displayCurrentTrip()
+    displayMenu()
+    entry = input("- ")
+    if entry == "S":
+        airport = input("")
+        stop = Stop_Transaction(stops,airport,airportGraph,distances)
+        tps.addTransaction(stop)
+    elif entry == "U":
+        tps.undoTransaction()
+    elif entry == "R":
+        tps.doTransaction()
+    elif entry == "E":
+        stops = []
+        tps.clearAllTransactions()
+    elif entry=="Q":
+        keepGoing = False
+print("GOOD BYE!! :((")
+
+
+
